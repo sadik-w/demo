b6dc8c48c666b55194cf810bd75b4cc2be397139
sadik <1171798741@qq.com>
2021-01-18 13:24:41 +0800

    first

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
 import router from './router'
+import './plugins/iview.js'
+import './plugins/element.js'
+import vuetify from './plugins/vuetify';
+import './plugins/ant-design-vue.js'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
